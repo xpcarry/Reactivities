637fe3a1171b03dc4e50821619ea13b6208a917a
abedynski <42570674+xpcarry@users.noreply.github.com>
2019-11-26 01:26:32 +0100

    Axios

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -8,13 +8,15 @@ interface IProps {
   activity: IActivity;
   createActivity: (activity: IActivity) => void;
   editActivity: (activity: IActivity) => void;
+  submitting: boolean;
 }
 
 export const ActivityForm: React.FC<IProps> = ({
   setEditMode,
   activity: initialFormState,
   createActivity,
-  editActivity
+  editActivity,
+  submitting
 }) => {
   const initializeForm = () => {
     if (initialFormState) {
@@ -83,7 +85,7 @@ export const ActivityForm: React.FC<IProps> = ({
         />
         <Form.Input name="city" onChange={handleInputChange} placeholder="City" value={activity.city} />
         <Form.Input name="venue" onChange={handleInputChange} placeholder="Venue" value={activity.venue} />
-        <Button floated="right" positive type="submit" content="Submit" />
+        <Button loading={submitting} floated="right" positive type="submit" content="Submit" />
         <Button
           onClick={() => setEditMode(false)}
           floated="right"
